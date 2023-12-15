673d9186de23c06b846b0b3c813005245b9a6433
Ali Shalabi <ashalabi13@gmail.com>
2023-12-14 23:10:29 -0800

    Simple problem

---
+++ coding_problems/area_of_country.py
@@ -0,0 +1,21 @@
+"""
+Create a function that takes a country's name and its area as arguments and returns the area of the country's proportion of the total world's landmass.
+
+Examples
+area_of_country("Russia", 17098242) ➞ "Russia is 11.48% of the total world's landmass"
+
+area_of_country("USA", 9372610), "USA is 6.29% of the total world's landmass"
+
+area_of_country("Iran", 1648195) ➞ "Iran is 1.11% of the total world's land
+
+Problem from: https://edabit.com/challenge/Cjtm4CpLzHDerQMfX
+"""
+
+def area_of_country(country_name, area):
+    total_area = 148940000
+    percentage = round(area/total_area * 100, 2)
+    return f"{country_name} is {percentage}% of the total world's landmass"
+
+print(area_of_country("Russia", 17098242))
+print(area_of_country("USA", 9372610))
+print(area_of_country("Iran", 1648195))
