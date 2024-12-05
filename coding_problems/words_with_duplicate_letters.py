71c68a322a1b05ddcb3e2879021fff7a136340a7
Ali Shalabi <ashalabi13@gmail.com>
2024-12-05 11:44:51 -0800

    words with duplicate letters

---
+++ coding_problems/words_with_duplicate_letters.py
@@ -0,0 +1,32 @@
+"""
+Given a common phrase, return False if any individual word in the phrase contains duplicate letters. Return True otherwise.
+
+Examples
+no_duplicate_letters("Fortune favours the bold.") ➞ True
+
+no_duplicate_letters("You can lead a horse to water, but you can't make him drink.") ➞ True
+
+no_duplicate_letters("Look before you leap.") ➞ False
+# Duplicate letters in "Look" and "before".
+
+no_duplicate_letters("An apple a day keeps the doctor away.") ➞ False
+# Duplicate letters in "apple", "keeps", "doctor", and "away".
+"""
+
+def no_duplicate_letters(sentence):
+    word_array = sentence.split(" ")
+    for word in word_array:
+        all_letters = set()
+        for letter in word:
+            # print(letter)
+            if letter in all_letters:
+                return False
+            else:
+                all_letters.add(letter)
+        # print(all_letters)
+    return True
+
+print(no_duplicate_letters("Fortune favours the bold."))
+print(no_duplicate_letters("You can lead a horse to water, but you can't make him drink."))
+print(no_duplicate_letters("Look before you leap."))
+print(no_duplicate_letters("An apple a day keeps the doctor away."))
