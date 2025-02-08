e87fae5d5e8a639d331166c7b625d597561471ce
Ali Shalabi <ashalabi13@gmail.com>
2025-02-07 18:49:56 -0800

    x and o, shuffle name

---
+++ coding_problems/x_and_o.py
@@ -0,0 +1,40 @@
+"""
+Create a function that takes a string, checks if it has the same number of "x"s and "o"s and returns either True or False.
+
+Return a boolean value (True or False).
+Return True if the amount of x's and o's are the same.
+Return False if they aren't the same amount.
+The string can contain any character.
+When "x" and "o" are not in the string, return True.
+Examples
+XO("ooxx") ➞ True
+
+XO("xooxx") ➞ False
+
+XO("ooxXm") ➞ True
+# Case insensitive.
+
+XO("zpzpzpp") ➞ True
+# Returns True if no x and o.
+
+XO("zzoo") ➞ False
+"""
+
+def XO(string):
+    x_count = 0
+    o_count = 0
+    for char in string:
+        if char.lower() == "x":
+            x_count += 1
+        if char.lower() == "o":
+            o_count += 1
+    if x_count == o_count:
+        return True
+    else:
+        return False
+
+print(XO("ooxx"))
+print(XO("xooxx"))
+print(XO("ooxXm"))
+print(XO("zpzpzpp"))
+print(XO("zzoo"))
