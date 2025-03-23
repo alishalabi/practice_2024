ebe4d22678e10cc1d30e316c54dfb6ebbf4f7fba
Ali Shalabi <ashalabi13@gmail.com>
2025-03-23 12:08:39 -0700

    issue with the replace function

---
+++ coding_problems/remove_letters.py
@@ -0,0 +1,26 @@
+"""
+Create a function that takes a list and string. The function should remove the letters in the string from the list, and return the list.
+
+Examples
+remove_letters(["s", "t", "r", "i", "n", "g", "w"], "string") ➞ ["w"]
+
+remove_letters(["b", "b", "l", "l", "g", "n", "o", "a", "w"], "balloon") ➞ ["b", "g", "w"]
+
+remove_letters(["d", "b", "t", "e", "a", "i"], "edabit") ➞ []
+"""
+
+def remove_letters(arr, word):
+    ret = []
+    my_word = word
+    # removed = []
+    for letter in arr:
+        if letter in my_word:
+            my_word.replace(letter, "", 1)
+            print(my_word)
+        else:
+            ret.append(letter)
+    return ret
+
+# print(remove_letters(["s", "t", "r", "i", "n", "g", "w"], "string"))
+print(remove_letters(["b", "b", "l", "l", "g", "n", "o", "a", "w"], "balloon"))
+# print(remove_letters(["d", "b", "t", "e", "a", "i"], "edabit"))
