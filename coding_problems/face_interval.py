f1d7b1e102a051a6cbfeec903ae65599600942b6
Ali Shalabi <ashalabi13@gmail.com>
2025-04-07 13:12:27 -0700

    Face interval

---
+++ coding_problems/face_interval.py
@@ -0,0 +1,33 @@
+"""
+In mathematics, interval is the difference between the largest number and the smallest number in a list.
+
+To illustrate:
+
+A = (3, 5, 7, 23, 11, 42, 80)
+
+Interval of A = 80 - 3 = 77
+Create a function that takes a list and returns ":)" if the interval of the list is equal to any other element; otherwise return ":(".
+
+Examples
+face_interval([1, 2, 5, 8, 3, 9]) ➞ ":)"
+# List interval is equal to one of the other elements.
+
+face_interval([5, 2, 8, 3, 11]) ➞ ":("
+# List interval is not among the other elements.
+
+face_interval("bruh") ➞ ":/"
+# "bruh" is not a list. It's string.
+"""
+
+def face_interval(arr):
+    if isinstance(arr, list) == False:
+        return ":/"
+    interval = max(arr) - min(arr)
+    if interval in arr:
+        return ":)"
+    else:
+        return ":("
+
+print(face_interval([1, 2, 5, 8, 3, 9]))
+print(face_interval([5, 2, 8, 3, 11]))
+print(face_interval("bruh"))
